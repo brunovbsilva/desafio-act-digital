f79189bf9b24751ddbff0c930e4f37fe647413b3
Bruno Silva <brunovinicius0704@gmail.com>
2025-10-14 21:26:55 -0300

    fix: routes and budgets for compose

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { ListProducts } from '@components/list-products/list-products';
 
 export const routes: Routes = [
-  { path: 'products', component: ListProducts },
-  { path: '**', redirectTo: 'products' },
+  { path: '', component: ListProducts },
+  { path: '**', redirectTo: '' },
 ];
